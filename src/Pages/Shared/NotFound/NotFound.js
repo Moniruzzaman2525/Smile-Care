7585c9639691dde80223e1b5a45d509178583859
unknown <md616264@gmail.com>
2022-04-18 13:03:05 +0600

    not found page work

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import notfound from '../../../images/404.png'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404</h2>
-            <p>Page Not Found</p>
+        <div className='container mx-auto'>
+            <div className='row'>
+                <div>
+                    <h2 className='fs-2'>ERROR 404</h2>
+                    <h2 className='fs-2'>NOT FOUND</h2></div>
+                <div>
+                    <img className='w-75' src={notfound} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
