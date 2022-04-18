b35db8097a63e419a6cac0c636121ffb1943e3aa
unknown <md616264@gmail.com>
2022-04-18 12:44:46 +0600

    about me add

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import monir from '../../images/monir.png'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About us</h2>
+        <div className='container d-flex justify-content-between align-items-center'>
+            <div>
+                <h2 className='text-fs-2'>I am Monir, my dream is to be a good developer.
+                    I will continue to work until I fulfill my dream.I'm a programming hero and a student. The programming hero paved the way for me to become a programmer.
+                    And my dream is not fulfilled, I will continue my efforts inshallah</h2>
+            </div>
+            <div className='border border-2'>
+                <img src={monir} alt="" />
+            </div>
         </div>
     );
 };
