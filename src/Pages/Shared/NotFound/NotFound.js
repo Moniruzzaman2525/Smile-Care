b1de6726353915f73ffe2c883ea71edcdb02aa8e
unknown <md616264@gmail.com>
2022-04-18 13:06:55 +0600

    not found page compleate

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -4,12 +4,12 @@ import notfound from '../../../images/404.png'
 const NotFound = () => {
     return (
         <div className='container mx-auto'>
-            <div className='row'>
+            <div className='d-flex justify-content-between align-items-center'>
+                <div className='ms-5'>
+                    <h2 className='fs-2 text-danger'>ERROR 404</h2>
+                    <h2 className='fs-2 text-danger'>NOT FOUND</h2></div>
                 <div>
-                    <h2 className='fs-2'>ERROR 404</h2>
-                    <h2 className='fs-2'>NOT FOUND</h2></div>
-                <div>
-                    <img className='w-75' src={notfound} alt="" />
+                    <img className='w-75 ms-5' src={notfound} alt="" />
                 </div>
             </div>
         </div>
