28af2b4ec7b9d9a227ef5c86d8135c917618ef9a
unknown <md616264@gmail.com>
2022-04-18 13:13:43 +0600

    extra part work

--- src/Pages/Home/ExtraPart/ExtraPart.js
+++ src/Pages/Home/ExtraPart/ExtraPart.js
@@ -5,8 +5,8 @@ import teat3 from '../../../images/trea-46.png'
 
 const ExtraPart = () => {
     return (
-        <div style={{ height: '220px' }} className='row'>
-            <div className='col-4 bg-success d-flex justify-content-between'>
+        <div className='row ms-1 me-1 mb-5 '>
+            <div className='col-sm-12 p-5 col-md-6 col-lg-4 bg-success d-flex justify-content-between'>
                 <div className='d-flex me-5 w-75 h-100 justify-content-center align-items-center'>
                     <img className='w-50' src={teat1} alt="" />
                 </div>
@@ -15,7 +15,7 @@ const ExtraPart = () => {
                     <p>There are only 2 dental specialties that only focus on dental esthetics: Prosthodontics and Orthodontics</p>
                 </div>
             </div>
-            <div className='col-4 bg-primary d-flex justify-content-between'>
+            <div className='col-sm-12 p-5 col-md-6 col-lg-4 bg-primary d-flex justify-content-between'>
                 <div className='d-flex w-75 h-100 justify-content-center align-items-center'>
                     <img className='w-50' src={teat2} alt="" />
                 </div>
@@ -24,7 +24,7 @@ const ExtraPart = () => {
                     <p>Bleaching methods use carbamide peroxide which reacts with water to form hydrogen peroxide lorem</p>
                 </div>
             </div>
-            <div className='col-4 bg-info d-flex justify-content-between'>
+            <div className='col-sm-12 p-5 col-md-6 col-lg-4 bg-info d-flex justify-content-between'>
                 <div className='d-flex w-75 h-100 justify-content-center align-items-center'>
                     <img className='w-50 rounded' src={teat3} alt="" />
                 </div>
