79f3f99cfd0572b610c48183620eccd41ff5d2b1
unknown <md616264@gmail.com>
2022-04-18 15:05:13 +0600

    not found page finish

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import notfound from '../../../images/404.png'
+import notfound from '../../../images/4041.jpg'
 
 const NotFound = () => {
     return (
         <div className='container mx-auto'>
             <div className='d-flex justify-content-between align-items-center'>
                 <div className='ms-5'>
-                    <h2 className='fs-2 text-danger'>ERROR 404</h2>
-                    <h2 className='fs-2 text-danger'>NOT FOUND</h2></div>
-                <div>
+                    <h2 className='fs-2 text-info'>ERROR 404</h2>
+                    <h2 className='fs-2 text-info'>NOT FOUND</h2></div>
+                <div className='d-flex justify-content-end'>
                     <img className='w-75 ms-5' src={notfound} alt="" />
                 </div>
             </div>
