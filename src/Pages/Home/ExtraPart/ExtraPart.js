f5ece73422ea86c6a571a9ef6cf6d4a5eec1f5f7
unknown <md616264@gmail.com>
2022-04-18 13:27:15 +0600

    extra page finish

--- src/Pages/Home/ExtraPart/ExtraPart.js
+++ src/Pages/Home/ExtraPart/ExtraPart.js
@@ -6,31 +6,31 @@ import teat3 from '../../../images/trea-46.png'
 const ExtraPart = () => {
     return (
         <div className='row ms-1 me-1 mb-5 '>
-            <div className='col-sm-12 p-5 col-md-6 col-lg-4 bg-success d-flex justify-content-between'>
+            <div style={{ backgroundColor: '#3fb6e0' }} className='col-sm-12 p-5 col-md-6 mb-2 col-lg-4 d-flex justify-content-between'>
                 <div className='d-flex me-5 w-75 h-100 justify-content-center align-items-center'>
                     <img className='w-50' src={teat1} alt="" />
                 </div>
                 <div>
-                    <h2 className='fs-3'>Tooth Protection</h2>
-                    <p>There are only 2 dental specialties that only focus on dental esthetics: Prosthodontics and Orthodontics</p>
+                    <h2 className='fs-3 text-light'>Tooth Protection</h2>
+                    <p className='text-light'>There are only 2 dental specialties that only focus on dental esthetics: Prosthodontics and Orthodontics</p>
                 </div>
             </div>
-            <div className='col-sm-12 p-5 col-md-6 col-lg-4 bg-primary d-flex justify-content-between'>
+            <div style={{ backgroundColor: '#a7c526' }} className='col-sm-12 p-5 col-md-6 mb-2 col-lg-4 d-flex justify-content-between'>
                 <div className='d-flex w-75 h-100 justify-content-center align-items-center'>
                     <img className='w-50' src={teat2} alt="" />
                 </div>
                 <div>
-                    <h2>Teeth Whitening</h2>
-                    <p>Bleaching methods use carbamide peroxide which reacts with water to form hydrogen peroxide lorem</p>
+                    <h2 className='fs-3 text-light'>Teeth Whitening</h2>
+                    <p className='text-light'>Bleaching methods use carbamide peroxide which reacts with water to form hydrogen peroxide lorem</p>
                 </div>
             </div>
-            <div className='col-sm-12 p-5 col-md-6 col-lg-4 bg-info d-flex justify-content-between'>
+            <div style={{ backgroundColor: '#333f48' }} className='col-sm-12 p-5 col-md-6 mb-2 col-lg-4 d-flex justify-content-between'>
                 <div className='d-flex w-75 h-100 justify-content-center align-items-center'>
                     <img className='w-50 rounded' src={teat3} alt="" />
                 </div>
                 <div>
-                    <h2>Cosmetic Dentistry</h2>
-                    <p>There are only 2 dental specialties that only focus on dental esthetics: Prosthodontics and Orthodontics</p>
+                    <h2 className='fs-3 text-light'>Cosmetic Dentistry</h2>
+                    <p className='text-light'>There are only 2 dental specialties that only focus on dental esthetics: Prosthodontics and Orthodontics</p>
                 </div>
             </div>
         </div>
