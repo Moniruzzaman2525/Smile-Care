d31aa66000e69115042e092e4fe0d6047b29eb8a
unknown <md616264@gmail.com>
2022-04-18 12:49:05 +0600

    about me page complate

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,13 +3,14 @@ import monir from '../../images/monir.png'
 
 const About = () => {
     return (
-        <div className='container d-flex justify-content-between align-items-center'>
+        <div className='container mt-5 d-flex justify-content-between align-items-center'>
             <div>
-                <h2 className='text-fs-2'>I am Monir, my dream is to be a good developer.
+                <h1 className='text-center'>My <span className='text-success'>Dream</span></h1>
+                <h3 style={{ color: '#787C83' }} className='text-fs-2 m-4 '>I am Monir, my dream is to be a good developer.
                     I will continue to work until I fulfill my dream.I'm a programming hero and a student. The programming hero paved the way for me to become a programmer.
-                    And my dream is not fulfilled, I will continue my efforts inshallah</h2>
+                    And my dream is not fulfilled, I will continue my efforts inshallah.</h3>
             </div>
-            <div className='border border-2'>
+            <div className='border border-2 rounded'>
                 <img src={monir} alt="" />
             </div>
         </div>
